9d5e90b1de84bbd64cf55193a7288510949c06aa
Dmytro Chaplynskyi <chaplinsky.dmitry@gmail.com>
2023-02-22 11:59:17 +0200

    Adding sample files for medium and small, updating convert_titles.py to make it work with arbitrary files

--- convert_titles.py
+++ convert_titles.py
@@ -1,15 +1,24 @@
+import argparse
 import json
 
-with open("expected.txt", "w") as fp_out_expected:
-    with open("predicted.txt", "w") as fp_out_predicted:
-        with open("out_topk=1.jsonl", "r") as fp_in:
-            for article in map(json.loads, fp_in):
-                fp_out_expected.write(article["title"].replace("\n", " ").strip() + "\n")
-                predicted = article["generated_title"].replace("\n", " ").strip()
-                if " рік: " in predicted:
-                    predicted, _ = predicted.split(" рік: ", 1)
+if __name__ == '__main__':
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description="Convert JSONL with original and generated titles into the format suitable for BLEURT and bert_score eval"
+    )
 
-                if " мітки: " in predicted:
-                    predicted, _ = predicted.split(" мітки: ", 1)
+    parser.add_argument("infile", type=argparse.FileType("r"), help="Input jsonl file")
+    parser.add_argument("out_reference", type=argparse.FileType("w"), help="Output file with original titles")
+    parser.add_argument("out_candidate", type=argparse.FileType("w"), help="Output file with candidate titles")
 
-                fp_out_predicted.write(predicted.strip() + "\n")
+    args: argparse.Namespace = parser.parse_args()
+
+    for article in map(json.loads, args.infile):
+        args.out_reference.write(article["title"].replace("\n", " ").strip() + "\n")
+        predicted = article["generated_title"].replace("\n", " ").strip()
+        if " рік: " in predicted:
+            predicted, _ = predicted.split(" рік: ", 1)
+
+        if " мітки: " in predicted:
+            predicted, _ = predicted.split(" мітки: ", 1)
+
+        args.out_candidate.write(predicted.strip() + "\n")
