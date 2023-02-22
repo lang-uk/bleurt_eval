c2a3658742ac139328ff9d22284dfc35c0219208
Dmytro Chaplynskyi <chaplinsky.dmitry@gmail.com>
2023-02-22 12:11:39 +0200

    Adding bleurt combined evaluation over medium and small candidates, amending changes to the combine script

--- combine_bleurt_eval.py
+++ combine_bleurt_eval.py
@@ -1,18 +1,32 @@
-# python -m bleurt.score_files   -candidate_file=predicted.txt   -reference_file=expected.txt  -bleurt_checkpoint=bleurt/BLEURT-20 -scores_file=scores
-
 import csv
+import argparse
+from typing import List
+from statistics import mean, median
 
-with open("expected.txt", "r") as fp_in_expected:
-    with open("predicted.txt", "r") as fp_in_predicted:
-        with open("scores", "r") as fp_in_scores:
-            with open("combined_bleurt_eval.csv", "w") as fp_out:
-                w = csv.DictWriter(fp_out, fieldnames=["reference", "candidate", "bleurt_score"])
-                w.writeheader()
-                for reference, candidate, bleurt_score in zip(fp_in_expected, fp_in_predicted, fp_in_scores):
-                    w.writerow(
-                        {
-                            "reference": reference.strip(),
-                            "candidate": candidate.strip(),
-                            "bleurt_score": bleurt_score.strip(),
-                        }
-                    )
+if __name__ == '__main__':
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description="Convert JSONL with original and generated titles into the format suitable for BLEURT and bert_score eval"
+    )
+
+    parser.add_argument("in_reference", type=argparse.FileType("r"), help="Input file with reference sentences")
+    parser.add_argument("in_candidate", type=argparse.FileType("r"), help="Input file with candidate sentences")
+    parser.add_argument("in_scores", type=argparse.FileType("r"), help="Input file with scores")
+    parser.add_argument("out_combined", type=argparse.FileType("w"), help="Combined output csv file")
+
+    args: argparse.Namespace = parser.parse_args()
+    scores: List[float] = []
+
+    w = csv.DictWriter(args.out_combined, fieldnames=["reference", "candidate", "bleurt_score"])
+    w.writeheader()
+    for reference, candidate, bleurt_score in zip(args.in_reference, args.in_candidate, args.in_scores):
+        score = float(bleurt_score.strip())
+        scores.append(score)
+        w.writerow(
+            {
+                "reference": reference.strip(),
+                "candidate": candidate.strip(),
+                "bleurt_score": score,
+            }
+        )
+
+    print(f"Samples {len(scores)}, mean: {mean(scores)}, median: {median(scores)}")
